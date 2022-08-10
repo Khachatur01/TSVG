e01488719555a3f58c9d9cb3551e0caea193207f
Yeritsyan Kh <xyeritsyan@mail.ru>
2022-08-10 12:26:20 +0000

    Fixed id parsing

--- projects/svg/src/element/ElementView.ts
+++ projects/svg/src/element/ElementView.ts
@@ -345,8 +345,8 @@ export abstract class ElementView implements Resizeable, Draggable, Drawable {
 
   public static parseId(id: string): {ownerId: string, index: number} {
     let idArray = id.split(/(_u|_e)+/);
-    let ownerId: string = idArray[1];
-    let index: number = parseInt(idArray[2]);
+    let ownerId: string = idArray[2];
+    let index: number = parseInt(idArray[4]);
 
     return {
       ownerId: ownerId,
